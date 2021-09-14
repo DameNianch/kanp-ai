9acc13a1e027512f4e0a9a799bad2609833ae4f2
kento-kobayashi <medaca.pro@gmail.com>
2021-09-15 00:14:16 +0900

    できた。あとはデータ分割をなんとかする

--- python/main/ml/main.py
+++ python/main/ml/main.py
@@ -1,29 +1,45 @@
-import tensorflow as tf
+import os
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import tensorflow_hub as hub
+import glob
+import random
+from data_util import load_images
 
 input_dim = [240, 240, 3]  # とりあえずpretrainモデルのドキュメントにあわせた。
-output_dim = 5  # 今回はご種類のペットボトル
+output_dim = 2  # 今回はご種類のペットボトル
 batch_size = 5  # 説明がめんどくさいので省略。一度に処理するデータ（画像枚）数
 epochs = 3  # デバッグ用。本学習では増やす。いっぱいあるほど基本的に良い。過学習に気をつける。
 model_path = "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet21k_ft1k_b1/feature_vector/2"
 
+mldata_dir = "mldata/train"
+target_class = ["cola", "calpico"]
+train_dir = [os.path.join(mldata_dir, i_target) for i_target in target_class]
+test_dir = train_dir  # TODO: なんとかする。
+
 
 class DummyGenerator:
-    def __init__(self, file_path):
-        self._file_path = file_path
+    def __init__(self, target_dir):
+        class_vector = np.eye(len(target_dir))
+        self._image_paths = []
+        self._image_classes = []
+        for i, i_dir in enumerate(target_dir):
+            for j_path in glob.glob(os.path.join(i_dir, "*")):
+                self._image_paths.append(j_path)
+                self._image_classes.append(class_vector[i])
+        self._image_indices = list(range(len(self._image_paths)))
 
     def __call__(self, batch_size):
         while True:
-            # ここでfile_pathから必要なデータを引っ張り出す
-            # 入力のaugmentを入れましょう。
-            yield np.ones((batch_size, *input_dim)), np.tile(np.arange(output_dim) / np.sum(np.arange(output_dim)), (batch_size, 1))
+            batch_indices = random.choices(self._image_indices, k=batch_size)
+            batch_classes = np.array([self._image_classes[i] for i in batch_indices])
+            batch_input = load_images([self._image_paths[i] for i in batch_indices])
+            yield batch_input, batch_classes
 
 
-train_generator = DummyGenerator("train.csv")
-test_generator = DummyGenerator("test.csv")
+train_generator = DummyGenerator(train_dir)
+test_generator = DummyGenerator(test_dir)
 
 
 def mame_model(output_dim):
